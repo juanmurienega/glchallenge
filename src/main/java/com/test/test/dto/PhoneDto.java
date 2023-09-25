e16b0fe04dabd0f7ebc61653b4173f30d07af6df
jmurienega <jmurienega@siltium.com>
2023-09-25 17:19:38 -0300

    app commit

---
+++ src/main/java/com/test/test/dto/PhoneDto.java
@@ -0,0 +1,37 @@
+package com.test.test.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class PhoneDto {
+	private String number;
+	private String citycode;
+	private String contrycode;
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getCitycode() {
+		return citycode;
+	}
+
+	public void setCitycode(String citycode) {
+		this.citycode = citycode;
+	}
+
+	public String getContrycode() {
+		return contrycode;
+	}
+
+	public void setContrycode(String contrycode) {
+		this.contrycode = contrycode;
+	}
+
+}
